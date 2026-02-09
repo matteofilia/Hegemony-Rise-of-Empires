b2130705c8fca55b98bef3a205d48edc412cb13f
Matteo Filia <matteo@matteofilia.com>
2026-02-09 11:52:36 -0500

    Fix function to translate game board index to x/y coordinates

--- game.js
+++ game.js
@@ -1,101 +1,103 @@
 class MainScene extends Phaser.Scene {
     constructor() {
-        super('MainScene');
+        super("MainScene");
     }
 
     preload() {
         // Load assets here
-        this.load.image('risk_map', 'risk.png');
+        this.load.image("risk_map", "risk.png");
     }
 
     create() {
         this.cam = this.cameras.main;
         this.zoomDirection = 1;
         this.cam.setZoom(1);
-        this.cam.setBounds(-400, -400, 800+800, 600+800);
-        
-        this.input.on('wheel', (pointer, gameObjects, deltaX, deltaY, deltaZ) => {
+        this.cam.setBounds(-400, -400, 800 + 800, 600 + 800);
+
+        this.input.on("wheel", (pointer, gameObjects, deltaX, deltaY, deltaZ) => {
             this.cam.zoom -= deltaY * 0.001;
             this.cam.zoom = Phaser.Math.Clamp(this.cam.zoom, 0.5, 2);
         });
-        
+
         // Enable dragging
-        this.input.on('pointerdown', (pointer) => {
+        this.input.on("pointerdown", (pointer) => {
             this.dragStartX = pointer.x + this.cam.scrollX;
             this.dragStartY = pointer.y + this.cam.scrollY;
             this.isDragging = true;
         });
 
-        this.input.on('pointerup', () => {
+        this.input.on("pointerup", () => {
             this.isDragging = false;
         });
 
-        this.input.on('pointermove', (pointer) => {
+        this.input.on("pointermove", (pointer) => {
             if (!this.isDragging) return;
             this.cam.scrollX = this.dragStartX - pointer.x;
             this.cam.scrollY = this.dragStartY - pointer.y;
         });
-        
-        this.add.text(400, 300, 'Hello World', {
-            fontSize: '32px',
-            color: '#ffffff'
-        }).setOrigin(0.5);
-        
+
+        this.add
+            .text(400, 300, "Hello World", {
+                fontSize: "32px",
+                color: "#ffffff"
+            })
+            .setOrigin(0.5);
+
         var green = 0x00bf00;
         var blue = 0x0000ff;
         var red = 0xff0000;
         var purple = 0xbd0bd3;
-        
+
         var light_blue = 0x0aebff;
         var brown = 0xc19540;
-        
+
         var light_grey = 0xd1d1d1;
         var dark_grey = 0x959595;
         var white = 0xffffff;
-        
+
         const text_style = {
-            fontSize: '16px',
-            color: '#ffffff',
-            fontFamily: 'monospace'
+            fontSize: "16px",
+            color: "#ffffff",
+            fontFamily: "monospace"
         };
-        
+
         const text_style_tiny = {
-            fontSize: '11px',
-            color: 'ffffff',
-            fontFamily: 'monospace'
+            fontSize: "11px",
+            color: "ffffff",
+            fontFamily: "monospace"
         };
-        
+
         const text_style_black = {
-            fontSize: '16px',
-            color: '000000',
-            fontFamily: 'monospace'
+            fontSize: "16px",
+            color: "000000",
+            fontFamily: "monospace"
         };
-        
-        this.convert_board_index_to_x_y = function(entity, board_index){
+
+        this.convert_board_index_to_x_y = function(entity, board_index) {
             board_index = board_index % 24;
             var coords = [];
-            
-            if (board_index >= 0 && board_index <= 7){
-                coords = [750-(board_index*100), 550];
-            } else if (board_index >= 8 && board_index <= 12){
-                coords = [50, 550-((board_index-8)*100)];
-            } else if (board_index >= 12 && board_index <= 19){
-                coords = [50, 550-((board_index-12)*100)];
-            } else if (board_index >= 19 && board_index <= 24){
-                coords = [50, 550-((board_index-19)*100)];
+
+            if (board_index >= 0 && board_index <= 7) {
+                coords = [750 - (board_index*100), 550];
+            } else if (board_index >= 7 && board_index <= 12) {
+                coords = [50, 550 - (board_index - 7) * 100];
+            } else if (board_index >= 12 && board_index <= 19) {
+                coords = [50 + (board_index - 12) * 100, 50];
+            } else if (board_index >= 19 && board_index <= 24) {
+                coords = [750, 50 + (board_index - 19) * 100];
             }
-            
+
             entity.x = coords[0];
             entity.y = coords[1];
         };
-        
+
         this.keys = this.input.keyboard.addKeys({
-            up: 'W',
-            down: 'S',
-            left: 'A',
-            right: 'D'
+            up: "W",
+            down: "S",
+            left: "A",
+            right: "D"
         });
-        
+
         const square0_1 = this.add.rectangle(50, 50, 100, 100, light_grey);
         const square0_2 = this.add.rectangle(150, 50, 100, 100, dark_grey);
         const square0_3 = this.add.rectangle(250, 50, 100, 100, light_grey);
@@ -104,7 +106,7 @@ class MainScene extends Phaser.Scene {
         const square0_6 = this.add.rectangle(550, 50, 100, 100, dark_grey);
         const square0_7 = this.add.rectangle(650, 50, 100, 100, light_grey);
         const square0_8 = this.add.rectangle(750, 50, 100, 100, dark_grey);
-        
+
         const square1_1 = this.add.rectangle(50, 550, 100, 100, dark_grey);
         const square1_2 = this.add.rectangle(150, 550, 100, 100, light_grey);
         const square1_3 = this.add.rectangle(250, 550, 100, 100, dark_grey);
@@ -113,102 +115,116 @@ class MainScene extends Phaser.Scene {
         const square1_6 = this.add.rectangle(550, 550, 100, 100, light_grey);
         const square1_7 = this.add.rectangle(650, 550, 100, 100, dark_grey);
         const square1_8 = this.add.rectangle(750, 550, 100, 100, light_grey);
-        
+
         const square2_1 = this.add.rectangle(50, 450, 100, 100, light_grey);
         const square2_2 = this.add.rectangle(50, 350, 100, 100, dark_grey);
         const square2_3 = this.add.rectangle(50, 250, 100, 100, light_grey);
         const square2_4 = this.add.rectangle(50, 150, 100, 100, dark_grey);
-        
+
         const square3_1 = this.add.rectangle(750, 450, 100, 100, dark_grey);
         const square3_2 = this.add.rectangle(750, 350, 100, 100, light_grey);
         const square3_3 = this.add.rectangle(750, 250, 100, 100, dark_grey);
         const square3_4 = this.add.rectangle(750, 150, 100, 100, light_grey);
-        
+
         const rect0_1 = this.add.rectangle(150, 87, 100, 25, green);
         const rect0_2 = this.add.rectangle(250, 87, 100, 25, green);
         const rect0_3 = this.add.rectangle(350, 87, 100, 25, green);
         const rect0_4 = this.add.rectangle(450, 87, 100, 25, white);
         const rect0_5 = this.add.rectangle(550, 87, 100, 25, blue);
         const rect0_6 = this.add.rectangle(650, 87, 100, 25, blue);
-        
-        const text0_1 = this.add.text(150, 87, 'New York', text_style).setOrigin(0.5); 
-        const text0_2 = this.add.text(250, 87, 'Houston', text_style).setOrigin(0.5); 
-        const text0_3 = this.add.text(350, 87, 'Chicago', text_style).setOrigin(0.5);
-        const text0_5 = this.add.text(550, 87, 'Tokyo', text_style).setOrigin(0.5);
-        const text0_6 = this.add.text(650, 87, 'Sydney', text_style).setOrigin(0.5);
-        
-        const text0_4 = this.add.text(450, 87, '???', text_style_black).setOrigin(0.5);
-        
+
+        const text0_1 = this.add.text(150, 87, "New York", text_style).setOrigin(0.5);
+        const text0_2 = this.add.text(250, 87, "Houston", text_style).setOrigin(0.5);
+        const text0_3 = this.add.text(350, 87, "Chicago", text_style).setOrigin(0.5);
+        const text0_5 = this.add.text(550, 87, "Tokyo", text_style).setOrigin(0.5);
+        const text0_6 = this.add.text(650, 87, "Sydney", text_style).setOrigin(0.5);
+
+        const text0_4 = this.add.text(450, 87, "???", text_style_black).setOrigin(0.5);
+
         const rect1_1 = this.add.rectangle(150, 513, 100, 25, purple);
         const rect1_2 = this.add.rectangle(250, 513, 100, 25, purple);
         const rect1_3 = this.add.rectangle(350, 513, 100, 25, white);
         const rect1_4 = this.add.rectangle(450, 513, 100, 25, red);
         const rect1_5 = this.add.rectangle(550, 513, 100, 25, red);
         const rect1_6 = this.add.rectangle(650, 513, 100, 25, red);
-        
-        const text1_1 = this.add.text(150, 513, 'Moscow', text_style).setOrigin(0.5); 
-        const text1_2 = this.add.text(250, 513, 'Kyiv', text_style).setOrigin(0.5); 
-        const text1_4 = this.add.text(450, 513, 'Montreal', text_style).setOrigin(0.5);
-        const text1_5 = this.add.text(550, 513, 'Toronto', text_style).setOrigin(0.5);
-        const text1_6 = this.add.text(650, 513, 'Ottawa', text_style).setOrigin(0.5);
-        
-        const text1_3 = this.add.text(350, 513, '???', text_style_black).setOrigin(0.5);
-        
+
+        const text1_1 = this.add.text(150, 513, "Moscow", text_style).setOrigin(0.5);
+        const text1_2 = this.add.text(250, 513, "Kyiv", text_style).setOrigin(0.5);
+        const text1_4 = this.add.text(450, 513, "Montreal", text_style).setOrigin(0.5);
+        const text1_5 = this.add.text(550, 513, "Toronto", text_style).setOrigin(0.5);
+        const text1_6 = this.add.text(650, 513, "Ottawa", text_style).setOrigin(0.5);
+
+        const text1_3 = this.add.text(350, 513, "???", text_style_black).setOrigin(0.5);
+
         const rect2_1 = this.add.rectangle(87, 150, 25, 100, brown);
         const rect2_2 = this.add.rectangle(87, 250, 25, 100, brown);
         const rect2_3 = this.add.rectangle(87, 350, 25, 100, brown);
         const rect2_4 = this.add.rectangle(87, 450, 25, 100, white);
-        
-        const text2_1 = this.add.text(87, 150, 'Shanghai', text_style).setOrigin(0.5).rotation = Phaser.Math.DegToRad(90); 
-        const text2_2 = this.add.text(87, 250, 'Shenzhen', text_style).setOrigin(0.5).rotation = Phaser.Math.DegToRad(90); 
-        const text2_3 = this.add.text(87, 350, 'Hong Kong', text_style).setOrigin(0.5).rotation = Phaser.Math.DegToRad(90);
-        
-        const text2_4 = this.add.text(87, 450, '???', text_style_black).setOrigin(0.5).rotation = Phaser.Math.DegToRad(90);
+
+        const text2_1 = (this.add.text(87, 150, "Shanghai", text_style).setOrigin(0.5).rotation =
+            Phaser.Math.DegToRad(90));
+        const text2_2 = (this.add.text(87, 250, "Shenzhen", text_style).setOrigin(0.5).rotation =
+            Phaser.Math.DegToRad(90));
+        const text2_3 = (this.add.text(87, 350, "Hong Kong", text_style).setOrigin(0.5).rotation =
+            Phaser.Math.DegToRad(90));
+
+        const text2_4 = (this.add.text(87, 450, "???", text_style_black).setOrigin(0.5).rotation =
+            Phaser.Math.DegToRad(90));
         
         const rect3_1 = this.add.rectangle(713, 150, 25, 100, white);
         const rect3_2 = this.add.rectangle(713, 250, 25, 100, light_blue);
         const rect3_3 = this.add.rectangle(713, 350, 25, 100, light_blue);
         const rect3_4 = this.add.rectangle(713, 450, 25, 100, light_blue);
-        
-        const text3_2 = this.add.text(713, 250, 'London', text_style).setOrigin(0.5).rotation = Phaser.Math.DegToRad(270); 
-        const text3_3 = this.add.text(713, 350, 'Paris', text_style).setOrigin(0.5).rotation = Phaser.Math.DegToRad(270); 
-        const text3_4 = this.add.text(713, 450, 'Rome', text_style).setOrigin(0.5).rotation = Phaser.Math.DegToRad(270);
-        
-        const text3_1 = this.add.text(713, 150, '???', text_style_black).setOrigin(0.5).rotation = Phaser.Math.DegToRad(270);
 
-        const text4_1 = this.add.text(750, 50, 'Cool', text_style_black).setOrigin(0.5).rotation = Phaser.Math.DegToRad(45);
-        const text5_1 = this.add.text(50, 50, 'Free Parking', text_style_black).setOrigin(0.5).rotation = Phaser.Math.DegToRad(45-90);
-        const text6_1 = this.add.text(50, 550, 'Cool', text_style_black).setOrigin(0.5).rotation = Phaser.Math.DegToRad(45);
-        const text7_1 = this.add.text(750, 550, 'GO', text_style_black).setOrigin(0.5).rotation = Phaser.Math.DegToRad(45+270);
-        
-        const risk_map = this.add.image(400, 300, 'risk_map');
+        const text3_2 = (this.add.text(713, 250, "London", text_style).setOrigin(0.5).rotation =
+            Phaser.Math.DegToRad(270));
+        const text3_3 = (this.add.text(713, 350, "Paris", text_style).setOrigin(0.5).rotation =
+            Phaser.Math.DegToRad(270));
+        const text3_4 = (this.add.text(713, 450, "Rome", text_style).setOrigin(0.5).rotation =
+            Phaser.Math.DegToRad(270));
+
+        const text3_1 = (this.add.text(713, 150, "???", text_style_black).setOrigin(0.5).rotation =
+            Phaser.Math.DegToRad(270));
+
+        const text4_1 = (this.add.text(750, 50, "Cool", text_style_black).setOrigin(0.5).rotation =
+            Phaser.Math.DegToRad(45));
+        const text5_1 = (this.add.text(50, 50, "Free Parking", text_style_black).setOrigin(0.5).rotation =
+            Phaser.Math.DegToRad(45 - 90));
+        const text6_1 = (this.add.text(50, 550, "Cool", text_style_black).setOrigin(0.5).rotation =
+            Phaser.Math.DegToRad(45));
+        const text7_1 = (this.add.text(750, 550, "GO", text_style_black).setOrigin(0.5).rotation = Phaser.Math.DegToRad(
+            45 + 270
+        ));
+
+        const risk_map = this.add.image(400, 300, "risk_map");
         risk_map.setOrigin(0.5);
-        
-        this.player_marker_1 = this.add.circle(750, 150+Phaser.Math.Between(-20, 20), 20, green);
-        const player_marker_2 = this.add.circle(750, 150+Phaser.Math.Between(-20, 20), 20, blue);
-        const player_marker_3 = this.add.circle(50, 350+Phaser.Math.Between(-20, 20), 20, light_blue);
-        const player_marker_4 = this.add.circle(50, 450+Phaser.Math.Between(-20, 20), 20, red);
-        const player_marker_5 = this.add.circle(150+Phaser.Math.Between(-20, 20), 50, 20, purple);
-        const player_marker_6 = this.add.circle(450+Phaser.Math.Between(-20, 20), 50, 20, brown);
-    
+
+        this.player_marker_1 = this.add.circle(750, 150 + Phaser.Math.Between(-20, 20), 20, green);
+        this.player_marker_2 = this.add.circle(750, 150 + Phaser.Math.Between(-20, 20), 20, blue);
+        this.player_marker_3 = this.add.circle(50, 350 + Phaser.Math.Between(-20, 20), 20, light_blue);
+        this.player_marker_4 = this.add.circle(50, 450 + Phaser.Math.Between(-20, 20), 20, red);
+        this.player_marker_5 = this.add.circle(150 + Phaser.Math.Between(-20, 20), 50, 20, purple);
+        this.player_marker_6 = this.add.circle(450 + Phaser.Math.Between(-20, 20), 50, 20, brown);
+
         const ui_rect_1 = this.add.rectangle(3100, 3100, 50, 50, green);
         ui_rect_1.setScrollFactor(1);
         ui_rect_1.setDepth(2);
         const ui_rect_2 = this.add.rectangle(3000, 3000, 50, 50, red);
-        
+
         const cam2 = this.cameras.add(0, 0, 200, 800);
         cam2.setBackgroundColor(0x5d5d5d);
         cam2.setZoom(1);
         cam2.setScroll(3000, 3000);
 
         this.board_game_index = 0;
-        
-        this.convert_board_index_to_x_y(player_marker_2, 0);
+
+        this.convert_board_index_to_x_y(this.player_marker_1, 1);
+        this.convert_board_index_to_x_y(this.player_marker_2, 0);
     }
 
     update(time, delta) {
         this.convert_board_index_to_x_y(this.player_marker_1, this.board_game_index);
-        
+
         if (Phaser.Input.Keyboard.JustDown(this.keys.up)) this.board_game_index += 1;
         if (Phaser.Input.Keyboard.JustDown(this.keys.down)) this.board_game_index -= 1;
     }
@@ -218,10 +234,10 @@ const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    backgroundColor: '#e3e3e3',
+    backgroundColor: "#e3e3e3",
     scene: MainScene,
     physics: {
-        default: 'arcade',
+        default: "arcade",
         arcade: {
             debug: false
         }
